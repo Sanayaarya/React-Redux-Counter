a8fa01d2abe7f6692f557f0db6fae5ccec9fc3ec
Sanaya Arya <sanayaarya0123@gmail.com>
2022-12-28 22:56:55 +0530

    Completed

---
+++ react-redux-counter/src/store/actions.js
@@ -0,0 +1,21 @@
+
+
+export function handleIncrement(step, limit) {
+  return {
+      type: 'increment',
+      step: step,
+      limit: limit,
+  };
+}
+export function handleDecrement(step, limit) {
+  return {
+      type: 'decrement',
+      step: step,
+      limit: limit,
+  };
+}
+export function reset() {
+  return {
+      type: 'reset',
+  };
+}
